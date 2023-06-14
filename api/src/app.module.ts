b096820fbce3db5b8847f1147bd1bd3341b4e280
Stephanie Guzman <sguzman247@gmail.com>
2023-06-14 15:17:50 -0500

    Add AppConfigurationModule to AppModule to connect to database

--- api/src/app.module.ts
+++ api/src/app.module.ts
@@ -1,9 +1,26 @@
+import { AppConfigurationModule } from './infrastructure/configuration/app-configuration.module';
+import { AppConfigurationService } from './infrastructure/configuration/app-configuration.service';
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { MongooseModule, MongooseModuleOptions } from '@nestjs/mongoose';
 
 @Module({
-  imports: [],
+  imports: [
+    AppConfigurationModule,
+    MongooseModule.forRootAsync({
+      imports: [AppConfigurationModule],
+      inject: [AppConfigurationService],
+      useFactory: (appConfigService: AppConfigurationService) => {
+        const options: MongooseModuleOptions = {
+          uri: appConfigService.connectionString,
+          useNewUrlParser: true,
+          useUnifiedTopology: true,
+        };
+        return options;
+      },
+    })
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
